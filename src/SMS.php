31dea34dbbf04b1128a889964dc2ced3212c7c91
Aria <aria@radmand.net>
2016-12-21 16:55:30 +0330

    successful sending message

--- src/SMS.php
+++ src/SMS.php
@@ -7,16 +7,19 @@ class SMS
 	{
 		$client = new \GuzzleHttp\Client();
 		$response = $client->request('GET', \Config::get('laravel-magfa-sms.url'), [
-			'service'      => [ 'enqueue' ],
-			'username'     => \Config::get('laravel-magfa-sms.username'),
-			'password'     => \Config::get('laravel-magfa-sms.password'),
-			'domain'       => \Config::get('laravel-magfa-sms.domain'),
-			'from'         => \Config::get('laravel-magfa-sms.from'),
-			'to'           => $receipent_number,
-			'message'      => 'Test Radmand',
-			'coding'       => '', // optional
-			'udh'          => '', // optional
-			'chkmessageid' => '', // optional
+			// 'debug' => true,
+			'query'  => [
+				'service'      => 'enqueue',
+				'username'     => \Config::get('laravel-magfa-sms.username'),
+				'password'     => \Config::get('laravel-magfa-sms.password'),
+				'domain'       => \Config::get('laravel-magfa-sms.domain'),
+				'from'         => \Config::get('laravel-magfa-sms.from'),
+				'to'           => $receipent_number,
+				'message'      => 'Test Radmand',
+				'coding'       => '', // optional
+				'udh'          => '', // optional
+				'chkmessageid' => '', // optional
+			]
 		]);
 
 		return $response;
